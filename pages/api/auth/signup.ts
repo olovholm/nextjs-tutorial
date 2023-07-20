c9e71f3d19d858f5f8d2b0e0a7a1dfd01b48f00a
Ola Løvholm <ola.lovholm@origo.oslo.kommune.no>
2023-07-20 10:23:51 +0200

    Create token

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -3,6 +3,7 @@ import validator from "validator"
 
 import {PrismaClient} from "@prisma/client"
 import bcrypt from "bcrypt"
+import * as jose from "jose"
 
 
 const prisma = new PrismaClient()
@@ -94,8 +95,15 @@ export default async function handler(
             }
         })
 
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+        const alg = "HS256"
+        const token = await new jose.SignJWT({
+            email: user.email
+        }).setProtectedHeader({alg}).setExpirationTime("24h")
+            .sign(secret)
+
         return res.status(200).json({
-            status: "OK"
+            token: token
         })
     }
 }
