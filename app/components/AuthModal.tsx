0e77f9ac029a83c26798fcf7e6d344cc88aa1412
Ola Løvholm <ola.lovholm@origo.oslo.kommune.no>
2023-07-25 14:22:59 +0200

    Submit deactivted if not all fields are filled out

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -4,8 +4,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import AuthModalInputs from "@/app/components/AuthModalInputs";
+import {signin} from "next-auth/core/routes";
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -43,6 +44,24 @@ export default function AuthModal({isSignin}: {isSignin : boolean}) {
         password: ""
     })
 
+    const [disabled, setDisabled] = useState(true)
+
+    useEffect(() => {
+        if(isSignin){
+            if(inputs.password && inputs.email){
+                return setDisabled(false)
+            }
+        } else {
+            if(inputs.firstName && inputs.lastName && inputs.email && inputs.phone && inputs.city && inputs.password){
+                return setDisabled(false)
+            }
+        }
+
+        setDisabled(true)
+
+
+    }, [inputs])
+
     return (
         <div>
             <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3" onClick={handleOpen}>
@@ -66,7 +85,7 @@ export default function AuthModal({isSignin}: {isSignin : boolean}) {
                                 {renderContent("Log into your account", "Create your open table account")}
                             </h2>
                             <AuthModalInputs inputs={inputs} handleChangeInputs={handleChangeInput} isSignin={isSignin} />
-                            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
+                            <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400" disabled={disabled}>
                                 {renderContent("Sign in", "Create account")}
                             </button>
                         </div>
