33711db0d5b79aa5588bb956296d7a467206bec3
Ola Løvholm <ola.lovholm@origo.oslo.kommune.no>
2023-07-25 14:03:45 +0200

    Moving auth into middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,5 +1,4 @@
 import {NextApiRequest, NextApiResponse} from "next"
-import * as jose from "jose"
 import jwt from "jsonwebtoken"
 import {PrismaClient} from "@prisma/client"
 
@@ -7,27 +6,8 @@ import {PrismaClient} from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse ){
-
     const bearerToken = req.headers["authorization"] as string
-
-    if(!bearerToken){
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
-
     const token = bearerToken.split(" ")[1]
-
-    if(!token){
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
-
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET)
-
-    try {
-        await jose.jwtVerify(token, secret)
-    } catch(error){
-        return res.status(401).json({errorMessage: "Unauthorized request"})
-    }
-
     const payload = jwt.decode(token) as {email: string }
 
     if(!payload.email){
